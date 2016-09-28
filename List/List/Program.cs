70787d7efb5941170154f579ac8181b6d0c96355
DucVu0801 <nvv190894@gmail.com>
2016-09-28 09:57:47 +0700

    Add Vu

--- List/List/Program.cs
+++ List/List/Program.cs
@@ -10,6 +10,21 @@ namespace List
     {
         static void Main(string[] args)
         {
+            Element<int> a = new Element<int>(5); 
+            Element<int> b = new Element<int>(8);
+            Element<int> c = new Element<int>(10);
+            List<int> list = new List<int>();
+            list.addfirst(a);
+            list.addfirst(b);
+            list.addfirst(c);
+            list.PrintList();
+            List<string> list2 = new List<string>();
+            list2.addfirst(new Element<string>("A"));
+            list2.addfirst(new Element<string>("B"));
+            list2.addfirst(new Element<string>("C"));
+            list2.PrintList();
+
+            Console.ReadKey();
         }
     }
 }
